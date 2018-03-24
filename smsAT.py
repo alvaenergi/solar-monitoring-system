4bef2b1c1f3e21dec57a6b80c0e68ed488504c27
alva.energi <the.alvaenergy@gmail.com>
2018-03-24 02:51:10 +0000

    init

---
+++ smsAT.py
@@ -0,0 +1,34 @@
+import serial
+import time
+
+recipient = "" 
+message = '{"id":"geranting","V":"1","I":"0"}'
+
+modem = serial.Serial("/dev/ttyACM0", 115200, timeout=5)
+
+modem.write('AT\r\n')
+time.sleep(0.5)
+modem.readline()
+data = modem.readline().strip('\r\n')
+
+# check if data is ok
+modem.write('AT+CMGF=1\r\n')
+time.sleep(0.5)
+modem.readline()
+data = modem.readline().strip('\r\n')
+
+if(data=='OK'):
+    modem.write('AT+CMGW=' + '\"' + recipient + '\"\r\n')
+    modem.readline()
+    modem.write(message + '\x1A\r\n')
+    time.sleep(0.5)
+    modem.readline()    
+    data = modem.readline().strip('\r\n')
+    
+    #code
+
+    code = data.split(" ")[1]
+
+    modem.write('AT+CMSS=' + code + '\r\n')
+
+
